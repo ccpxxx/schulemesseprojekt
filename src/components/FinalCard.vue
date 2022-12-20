7edac0c092a7d73cfd2ceb36702444490a44674b
xiroli <davidmazo98@gmail.com>
2022-12-20 21:58:33 +0100

    yee im lost lmao

--- src/components/FinalCard.vue
+++ src/components/FinalCard.vue
@@ -3,14 +3,14 @@
         <div class="card-container__left flex p-4 justify-center rounded-full ">
             
                 <img :src="obj.picture"
-                class="moveIn h-full w-3/4 object-cover rounded-full" alt="user picture " />
+                class="moveIn h-[429px] w-[429px] object-cover rounded-full" alt="user picture " />
            
         </div>
         <div class="card-container__right p-4">
             <div class="card-container__right__text border border-gray-800 rounded-sm">
-                <h1 class="h-20 text-4xl">Name: {{ obj.name }}</h1>
-                <h2 class="h-20 text-4xl" >ID: {{ obj.id }}</h2>
-                <h2 class="h-20 text-4xl">ROLE: BLABLA</h2>
+                <h1 class="h-20 text-4xl">Vorname: {{ obj.name }}</h1>
+                <h2 class="h-20 text-4xl" >Nachname: Mustermann</h2>
+                <h2 class="h-20 text-4xl">Rolle: BLABLA</h2>
                 <h2 class="h-20 text-4xl">ROLE: BLABLA</h2>
                 <h2 class="h-20 text-4xl">ROLE: BLABLA</h2>
             </div>
@@ -32,7 +32,7 @@ const props = defineProps<{ obj: { id: string, name: string, picture: string } }
     @keyframes moveIn {
         0% {
             
-            transform:  translateX(250px);
+            transform:  translateX(150px);
         }
         100% {
             
--- src/components/PictureUpload.vue
+++ src/components/PictureUpload.vue
@@ -1,9 +1,19 @@
 <template>
     <div class="flex justify-center items-center gap-4">
         <div class="pseudo-container overflow-hidden h-[429px] w-[429px] rounded-full bg-slate-200">
-            <img class="picture  object-cover" :src="xaxa" />
+            <!-- <img class="picture  object-cover" :src="xaxa" /> -->
+            <video class="h-full w-full" autoplay="true" id="videoElement" ref="video">
+                <canvas id="canvas" width="429" height="429"></canvas>
+</video>
+<img :src="imgsrc" class="object-cover" style="position: absolute;
+    height: 429px;
+    width: 429px;
+    border-radius: 100%;
+    top: 48.5%;" />
+
         </div>
         <button @click="back">Back</button>
+        <button @click="snap">Snap</button>
         <button @click="emitting">Emit</button>
     </div>
 </template>
@@ -12,11 +22,36 @@
 import xaxa from "../assets/xaxa123.jpg"
 import { triggerEvent } from "../utils/func";
 const emit = defineEmits<(event: string, value: string) => void>()
+const imgsrc = $ref("")
 const emitting = () => {
-    emit('picture', xaxa)
+    emit('picture', imgsrc)
 }
 const back = () => {
     triggerEvent(document.querySelector(".contact"), "click")
+}
+
+
+var video = $ref(document.querySelector("#videoElement"));
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video!.srcObject = stream;
+    })
+    .catch(function (err) {
+      console.log("Something went wrong!", err);
+    });
+}
+
+function snap() {
+    let canvas = document.querySelector("#canvas") as HTMLCanvasElement;
+   	canvas!.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+   	let image_data_url = canvas.toDataURL('image/jpeg');
+    imgsrc = image_data_url           
+    // emit('picture', image_data_url)
+
+   	// data url of the image
+   	console.log(image_data_url);
 }
 </script>
 
