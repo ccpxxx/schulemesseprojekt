23a21e12f64eaa00061a9d8ff112da7062d4775a
ccpxxx <nicklas.segedi@its-stuttgart.de>
2023-01-09 20:22:29 +0100

    MongoDB, Offline Func, docker things

--- backend/Customer.cs
+++ backend/Customer.cs
@@ -1,10 +1,14 @@
-﻿namespace Messe
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Messe
 {
     public class Customer
     {
 
         public string firstname { get; set; }
         public string name { get; set; }
+        [BsonId]
         public int id { get; set; }
         public string picture { get; set; }
 
